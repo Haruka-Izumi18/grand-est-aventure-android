0f48970cc36e2261365b9cfcadef455b2460080f
Haruka-Izumi18 <haruka.izumi18@gmail.com>
2026-05-06 17:10:41 +0200

    index.tsx

--- myApp/app/(auth)/sign-in.tsx
+++ myApp/app/(auth)/sign-in.tsx
@@ -1,1 +1,12 @@
 import { useState  } from "react";
+import { View, Text } from "react-native";
+import { router, Link } from "expo-router";
+import { authClient } from "@/lib/auth-client";
+
+export default function BasicFormExample() {
+  return (
+  <View>
+    <Text>Balad&apos;indice </Text>
+  </View>
+  );
+}
